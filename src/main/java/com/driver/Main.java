db3a4c512a92c2dd5e5ea5cdd352dd6b68622f29
Anita <anitaghanata@gmail.com>
2023-04-11 21:42:10 +0530

    Product class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
 
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+    public void main(String[] args) {
+        int x= 1;
+        int y= 2;
+        int z= 3;
+        Product p= new Product();
+        p.product(x, y);
+        p.product(x, y, z);
+        p.product(x, y);
+    }
 }
